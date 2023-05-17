6393de4d46c71ab3ad4c3d82eb3c1fed0f51db47
doailabs <128745776+doailabs@users.noreply.github.com>
2023-05-17 16:26:31 +0200

    Create downloadCSV.js

---
+++ downloadCSV.js
@@ -0,0 +1,22 @@
+function downloadCSV(downloadId) {
+    // Acceder a la API de Genesys Cloud
+    const downloadsApi = new platformClient.DownloadsApi();
+    
+    downloadsApi.getDownload(downloadId)
+        .then(response => {
+            // Convertir la respuesta a un archivo CSV
+            let csvContent = 'data:text/csv;charset=utf-8,' + response;
+            let encodedUri = encodeURI(csvContent);
+            let link = document.createElement("a");
+            link.setAttribute("href", encodedUri);
+            link.setAttribute("download", "my_data.csv");
+            document.body.appendChild(link); // Required for FF
+            link.click(); // Descargar el archivo CSV
+            
+            // Llamar a la función draw y pasar el archivo CSV
+            draw(encodedUri);
+        })
+        .catch((err) => {
+            console.log(`There was a failure calling downloadCSV. Error: ${err}`);
+        });
+}
